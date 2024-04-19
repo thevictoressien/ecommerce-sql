3b3ac4cc30358ca7ac37dcad79ae327dbc74e0a5
Victor Essien <86724260+thevictoressien@users.noreply.github.com>
2024-04-19 10:12:46 +0100

    Refactor: Update init.sql

--- infra_setup/init.sql
+++ infra_setup/init.sql
@@ -1,60 +1,67 @@
 
 -- Create schema
-CREATE SCHEMA IF NOT EXISTS ALT_SCHOOL;
+CREATE SCHEMA IF NOT EXISTS ECOMM;
 
 
--- create and populate tables
-create table if not exists ALT_SCHOOL.PRODUCTS
+-- setup the products table
+create table if not exists ECOMM.PRODUCTS
 (
     id  serial primary key,
     name varchar not null,
     price numeric(10, 2) not null
 );
 
-
-COPY ALT_SCHOOL.PRODUCTS (id, name, price)
+COPY ECOMM.PRODUCTS (id, name, price)
 FROM '/data/products.csv' DELIMITER ',' CSV HEADER;
 
--- setup customers table following the example above
 
--- TODO: Provide the DDL statment to create this table ALT_SCHOOL.CUSTOMERS
+-- setup the customers table 
+create table if not exists ECOMM.CUSTOMERS
+(
+    customer_id uuid not null primary key, 
+    device_id uuid not null,
+    location varchar not null,
+    currency varchar not null
+);
 
--- TODO: provide the command to copy the customers data in the /data folder into ALT_SCHOOL.CUSTOMERS
+COPY ECOMM.CUSTOMERS (customer_id, device_id, location, currency)
+FROM '/data/customers.csv' DELIMITER ',' CSV HEADER;
 
 
-
--- TODO: complete the table DDL statement
-create table if not exists ALT_SCHOOL.ORDERS
+-- setup the orders table 
+create table if not exists ECOMM.ORDERS
 (
     order_id uuid not null primary key,
-    -- provide the other fields
+    customer_id uuid not null, 
+    status varchar not null,
+    checked_out_at timestamp not null
 );
 
+COPY ECOMM.ORDERS (order_id, customer_id, status, checked_out_at)
+FROM '/data/orders.csv' DELIMITER ',' CSV HEADER;
 
--- provide the command to copy orders data into POSTGRES
 
-
-create table if not exists ALT_SCHOOL.LINE_ITEMS
+-- setup the line_items table 
+create table if not exists ECOMM.LINE_ITEMS
 (
     line_item_id serial primary key,
-    -- provide the remaining fields
+    order_id uuid not null,
+    item_id int not null,
+    quantity int not null
 );
 
+COPY ECOMM.LINE_ITEMS (line_item_id, order_id, item_id, quantity)
+FROM '/data/line_items.csv' DELIMITER ',' CSV HEADER;
 
--- provide the command to copy ALT_SCHOOL.LINE_ITEMS data into POSTGRES
 
-
--- setup the events table following the examle provided
-create table if not exists ALT_SCHOOL.EVENTS
+-- setup the events table 
+create table if not exists ECOMM.EVENTS
 (
-    -- TODO: PROVIDE THE FIELDS
+    event_id serial primary key,
+    customer_id uuid not null,
+    event_data jsonb,
+    event_timestamp timestamp
 );
-
--- TODO: provide the command to copy ALT_SCHOOL.EVENTS data into POSTGRES
-
-
-
-
-
-
-
+ 
+COPY ECOMM.EVENTS (event_id, customer_id, event_data, event_timestamp)
+FROM '/data/events.csv' DELIMITER ',' CSV HEADER;
